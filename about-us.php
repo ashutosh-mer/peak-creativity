c15445013380346173bc9fc7bea3faca634dc39c
Ashutosh Mer <ashutoshmer@gmail.com>
2023-10-15 18:02:35 +0530

    About us page initialization

---
+++ about-us.php
@@ -0,0 +1,191 @@
+<!doctype html>
+<html>
+
+<head>
+	<?php include("include/head.php"); ?>
+</head>
+
+<body>
+
+	<?php include("include/footer.php"); ?>
+	<!-- cta-sec-starts -->
+	<div class="cta-sec-wrap section">
+		<div class="container">
+			<div class="content">
+				<h2 class="h2 title">Let’s discuss the idea</h2>
+				<p class="desc">
+					Quisque maximus vestibulum orci, non auctor libero facilisis congue.
+					Vivamus metus lorem, tempus nec mauris sed, scelerisque congue metus.
+					Nulla enim dui, rhoncus in tristique in, feugiat iaculis mi.
+				</p>
+				<div class="btn-wrap">
+					<a href="#" class="btn-icon btn btn-primary">
+						Talk to Sales
+						<svg class="icon">
+							<use xlink:href="./assets/images/icons/svgsprite.svg#peak-icon"></use>
+						</svg>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- cta-sec-ends -->
+	
+
+	<!-- our-offering-starts -->
+	<div class="our-offering-wrap section dark-bg">
+		<div class="container">
+			<div class="content-group">
+				<h2 class="h2 title">
+					What We Offer
+				</h2>
+				<div class="desc">
+					<p>
+						Risus commodo id odio turpis pharetra elementum. Pulvinar porta porta feugiat scelerisque in
+						elit.
+						Morbi rhoncus, tellus, eros consequat magna semper orci a tincidunt.
+					</p>
+				</div>
+			</div>
+
+			<ul class="icon-card-listing">
+				<li>
+					<div class="card">
+						<div class="icon-wrap">
+							<img src="assets/images/offering-icon.png" alt="offering-icon" width="48" height="48">
+						</div>
+						<div class="content">
+							<h3 class="h3 card-title">
+								Branding
+							</h3>
+							<p class="desc">
+								Egestas tellus nunc proin amet tellus tincidunt lacus consequat. Ultrices
+								Egestas tellus nunc proin amet tellus tincidunt lacus consequat. Ultrices
+								Egestas tellus nunc proin amet tellus tincidunt lacus consequat. Ultrices
+							</p>
+						</div>
+					</div>
+				</li>
+
+
+				<li>
+					<div class="card">
+						<div class="icon-wrap">
+							<img src="assets/images/offering-icon.png" alt="offering-icon" width="48" height="48">
+						</div>
+						<div class="content">
+							<h3 class="h3 card-title">
+								Branding
+							</h3>
+							<p class="desc">
+								Egestas tellus nunc proin amet tellus tincidunt lacus consequat. Ultrices
+							</p>
+						</div>
+					</div>
+				</li>
+
+
+				<li>
+					<div class="card">
+						<div class="icon-wrap">
+							<img src="assets/images/offering-icon.png" alt="offering-icon" width="48" height="48">
+						</div>
+						<div class="content">
+							<h3 class="h3 card-title">
+								Branding
+							</h3>
+							<p class="desc">
+								Egestas tellus nunc proin amet tellus tincidunt lacus consequat. Ultrices
+							</p>
+						</div>
+					</div>
+				</li>
+
+
+				<li>
+					<div class="card">
+						<div class="icon-wrap">
+							<img src="assets/images/offering-icon.png" alt="offering-icon" width="48" height="48">
+						</div>
+						<div class="content">
+							<h3 class="h3 card-title">
+								Branding
+							</h3>
+							<p class="desc">
+								Egestas tellus nunc proin amet tellus tincidunt lacus consequat. Ultrices
+							</p>
+						</div>
+					</div>
+				</li>
+
+
+				<li>
+					<div class="card">
+						<div class="icon-wrap">
+							<img src="assets/images/offering-icon.png" alt="offering-icon" width="48" height="48">
+						</div>
+						<div class="content">
+							<h3 class="h3 card-title">
+								Branding
+							</h3>
+							<p class="desc">
+								Egestas tellus nunc proin amet tellus tincidunt lacus consequat. Ultrices
+							</p>
+						</div>
+					</div>
+				</li>
+
+				<li>
+					<div class="card">
+						<div class="icon-wrap">
+							<img src="assets/images/offering-icon.png" alt="offering-icon" width="48" height="48">
+						</div>
+						<div class="content">
+							<h3 class="h3 card-title">
+								Branding
+							</h3>
+							<p class="desc">
+								Egestas tellus nunc proin amet tellus tincidunt lacus consequat. Ultrices
+							</p>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<!-- our-offering-ends -->
+
+	<!-- inner-banner-starts -->
+	<div class="inner-banner-wrap last-sec" id="first-sec">
+		<div class="container">
+			<div class="content">
+                <div class="tag">
+                About Us
+                </div>
+				<h1 class="h1 title">
+					Attract <span class="gradient-text">
+						NewLeads
+					</span> like never before
+				</h1>
+				<p class="desc">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ut cumque enim error soluta mollitia veniam quisquam repudiandae, fuga quaerat ipsa assumenda doloremque commodi ad illo rerum, quas dolorum voluptates.
+				</p>
+				<div class="btn-wrap">
+					<a href="#second-sec" class="btn btn-primary btn-icon offset-top">
+						Transform
+						<svg class="icon">
+							<use xlink:href="./assets/images/icons/svgsprite.svg#peak-icon"></use>
+						</svg>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- inner-banner-ends -->
+
+
+
+	<?php include("include/header.php"); ?>
+</body>
+
+</html>
