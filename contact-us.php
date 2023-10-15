7cd90ab99b1ffff4d5ee001a48463d67080f062f
Ashutosh Mer <ashutoshmer@gmail.com>
2023-10-16 00:08:46 +0530

    About us complete, contact us start

---
+++ contact-us.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html>
+
+<head>
+    <?php include("include/head.php"); ?>
+</head>
+
+<body>
+
+    <?php include("include/footer.php"); ?>
+    <!-- cta-sec-starts -->
+    <div class="cta-sec-wrap section">
+        <div class="container">
+            <div class="content">
+                <h2 class="h2 title">Let’s discuss the idea</h2>
+                <p class="desc">
+                    Quisque maximus vestibulum orci, non auctor libero facilisis congue.
+                    Vivamus metus lorem, tempus nec mauris sed, scelerisque congue metus.
+                    Nulla enim dui, rhoncus in tristique in, feugiat iaculis mi.
+                </p>
+                <div class="btn-wrap">
+                    <a href="#" class="btn-icon btn btn-primary">
+                        Talk to Sales
+                        <svg class="icon">
+                            <use xlink:href="./assets/images/icons/svgsprite.svg#peak-icon"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- cta-sec-ends -->
+
+    <!-- additional-resource-sec-starts -->
+    <div class="additional-resource-wrap section dark-bg">
+        <div class="container">
+            <div class="content">
+                <div class="sec-title text-center">
+                    <h2 class="h2 title">From The Blog</h2>
+                </div>
+
+                <div class="row blog-listing">
+                    <div class="col-lg-4">
+                        <div class="blog-card">
+                            <div class="inner-box">
+                                <div class="card">
+                                    <div class="blog-image">
+                                        <a href="#">
+                                            <img src="assets/images/work-card-img.png" alt="thumb" width="442"
+                                                height="240">
+                                        </a>
+                                    </div>
+                                    <div class="blog-content">
+                                        <div class="date">March 23, 2023</div>
+                                        <h5 class="h5 blog-title">
+                                            <a href="#">
+                                                Customized NetSuite Comparative Reports for Improved
+                                                Business Analysis
+                                            </a>
+                                        </h5>
+                                        <div class="btn-wrap">
+                                            <a href="#" class="btn-link">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="blog-card border-box">
+                            <div class="inner-box">
+                                <div class="card">
+                                    <div class="blog-image">
+                                        <a href="#">
+                                            <img src="assets/images/work-card-img.png" alt="thumb" width="442"
+                                                height="240">
+                                        </a>
+                                    </div>
+                                    <div class="blog-content">
+                                        <div class="date">March 23, 2023</div>
+                                        <h5 class="h5 blog-title">
+                                            <a href="#">
+                                                Customized NetSuite Comparative Reports for Improved
+                                                Business Analysis
+                                            </a>
+                                        </h5>
+                                        <div class="btn-wrap">
+                                            <a href="#" class="btn-link">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="blog-card border-box">
+                            <div class="inner-box">
+                                <div class="card">
+                                    <div class="blog-image">
+                                        <a href="#">
+                                            <img src="assets/images/work-card-img.png" alt="thumb" width="442"
+                                                height="240">
+                                        </a>
+                                    </div>
+                                    <div class="blog-content">
+                                        <div class="date">March 23, 2023</div>
+                                        <h5 class="h5 blog-title">
+                                            <a href="#">
+                                                Customized NetSuite Comparative Reports for Improved
+                                                Business Analysis
+                                            </a>
+                                        </h5>
+                                        <div class="btn-wrap">
+                                            <a href="#" class="btn-link">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- additional-resource-sec-ends -->
+
+    <!-- contact-us-sec-starts -->
+    <div class="contact-us-sec-wrap">
+        <div class="container">
+            <div class="content">
+                <div class="contact-box">
+                    <div class="contact-details">
+
+                    </div>
+                    <div class="main-form">
+                        <form>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label class="form-label">
+                                            First name
+                                        </label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label class="form-label">
+                                            Last name
+                                        </label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label class="form-label">
+                                            Email
+                                        </label>
+                                        <input type="email" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label class="form-label">
+                                            Phone
+                                        </label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-12">
+                                    .
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- contact-us-sec-ends -->
+
+    <!-- inner-banner-starts -->
+    <div class="inner-banner-wrap last-sec" id="first-sec">
+        <div class="container">
+            <div class="content">
+                <div class="tag">
+                    Contact Us
+                </div>
+                <h1 class="h1 title">
+                    Attract <span class="gradient-text">
+                        NewLeads
+                    </span> like never before
+                </h1>
+                <p class="desc">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ut cumque enim error soluta mollitia
+                    veniam quisquam repudiandae, fuga quaerat ipsa assumenda doloremque commodi ad illo rerum, quas
+                    dolorum voluptates.
+                </p>
+                <div class="btn-wrap">
+                    <a href="#second-sec" class="btn btn-primary btn-icon offset-top">
+                        Transform
+                        <svg class="icon">
+                            <use xlink:href="./assets/images/icons/svgsprite.svg#peak-icon"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- inner-banner-ends -->
+
+
+
+    <?php include("include/header.php"); ?>
+</body>
+
+</html>
