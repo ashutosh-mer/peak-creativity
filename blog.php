3176c0fb598446f4d5435dc949d8da701f4436ed
Ashutosh Mer <ashutoshmer@gmail.com>
2023-11-02 01:37:40 +0530

    Mutiple pages conversion - blog/details/service

---
+++ blog.php
@@ -0,0 +1,392 @@
+<!doctype html>
+<html>
+
+<head>
+    <?php include("include/head.php"); ?>
+</head>
+
+<body>
+
+    <?php include("include/footer.php"); ?>
+    <!-- cta-sec-starts -->
+    <div class="cta-sec-wrap section">
+        <div class="container">
+            <div class="content">
+                <h2 class="h2 title">Let’s discuss the idea</h2>
+                <p class="desc">
+                    Quisque maximus vestibulum orci, non auctor libero facilisis congue.
+                    Vivamus metus lorem, tempus nec mauris sed, scelerisque congue metus.
+                    Nulla enim dui, rhoncus in tristique in, feugiat iaculis mi.
+                </p>
+                <div class="btn-wrap">
+                    <a href="#" class="btn-icon btn btn-primary">
+                        Talk to Sales
+                        <svg class="icon">
+                            <use xlink:href="./assets/images/icons/svgsprite.svg#peak-icon"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- cta-sec-ends -->
+
+    <div class="featured-post-wrap section" id="second-sec">
+        <div class="container">
+            <div class="content">
+                <div class="sec-title">
+                    <h2 class="h2 title">Featured Post</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-7">
+                        <div class="featured-blog-lg">
+                            <a href="#">
+                                <div class="img-box">
+                                    <img src="assets/images/work-card-img.png" alt="featured-blog" width="791"
+                                        height="431">
+                                </div>
+                            </a>
+                            <div class="content">
+                                <div class="date">March 23, 2023</div>
+                                <a href="#">
+                                    <h4 class="h4 title">
+                                        The Importance of Tracking Dead Products in NetSuite and
+                                        How to Create a Custom Report
+                                    </h4>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5">
+                        <ul class="blog-listing-sm">
+                            <li>
+                                <div class="left">
+                                    <a href="#">
+                                        <div class="img-box">
+                                            <img src="assets/images/work-card-img.png" alt="blog-sm" width="118"
+                                                height="118">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="right">
+                                    <div class="date">March 9, 2023</div>
+                                    <a href="#">
+                                        <h6 class="h6 title">
+                                            Improving Sales Orders Creation Experience with NetSuite
+                                            Customer Center
+                                        </h6>
+                                    </a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="left">
+                                    <a href="#">
+                                        <div class="img-box">
+                                            <img src="assets/images/work-card-img.png" alt="blog-sm" width="118"
+                                                height="118">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="right">
+                                    <div class="date">March 9, 2023</div>
+                                    <a href="#">
+                                        <h6 class="h6 title">
+                                            Improving Sales Orders Creation Experience with NetSuite
+                                            Customer Center
+                                        </h6>
+                                    </a>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div class="left">
+                                    <a href="#">
+                                        <div class="img-box">
+                                            <img src="assets/images/work-card-img.png" alt="blog-sm" width="118"
+                                                height="118">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="right">
+                                    <div class="date">March 9, 2023</div>
+                                    <a href="#">
+                                        <h6 class="h6 title">
+                                            Improving Sales Orders Creation Experience with NetSuite
+                                            Customer Center
+                                        </h6>
+                                    </a>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div class="left">
+                                    <a href="#">
+                                        <div class="img-box">
+                                            <img src="assets/images/work-card-img.png" alt="blog-sm" width="118"
+                                                height="118">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="right">
+                                    <div class="date">March 9, 2023</div>
+                                    <a href="#">
+                                        <h6 class="h6 title">
+                                            Improving Sales Orders Creation Experience with NetSuite
+                                            Customer Center
+                                        </h6>
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="row blog-listing">
+                    <div class="col-lg-4">
+                        <div class="blog-card border-box">
+                            <div class="inner-box">
+                                <div class="card">
+                                    <div class="blog-image">
+                                        <a href="#">
+                                            <img src="assets/images/work-card-img.png" alt="thumb" width="442"
+                                                height="240">
+                                        </a>
+                                    </div>
+                                    <div class="blog-content">
+                                        <div class="date">March 23, 2023</div>
+                                        <h5 class="h5 blog-title">
+                                            <a href="#">
+                                                Customized NetSuite Comparative Reports for Improved
+                                                Business Analysis
+                                            </a>
+                                        </h5>
+                                        <div class="btn-wrap">
+                                            <a href="#" class="btn-link">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="blog-card border-box">
+                            <div class="inner-box">
+                                <div class="card">
+                                    <div class="blog-image">
+                                        <a href="#">
+                                            <img src="assets/images/work-card-img.png" alt="thumb" width="442"
+                                                height="240">
+                                        </a>
+                                    </div>
+                                    <div class="blog-content">
+                                        <div class="date">March 23, 2023</div>
+                                        <h5 class="h5 blog-title">
+                                            <a href="#">
+                                                Customized NetSuite Comparative Reports for Improved
+                                                Business Analysis
+                                            </a>
+                                        </h5>
+                                        <div class="btn-wrap">
+                                            <a href="#" class="btn-link">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="blog-card border-box">
+                            <div class="inner-box">
+                                <div class="card">
+                                    <div class="blog-image">
+                                        <a href="#">
+                                            <img src="assets/images/work-card-img.png" alt="thumb" width="442"
+                                                height="240">
+                                        </a>
+                                    </div>
+                                    <div class="blog-content">
+                                        <div class="date">March 23, 2023</div>
+                                        <h5 class="h5 blog-title">
+                                            <a href="#">
+                                                Customized NetSuite Comparative Reports for Improved
+                                                Business Analysis
+                                            </a>
+                                        </h5>
+                                        <div class="btn-wrap">
+                                            <a href="#" class="btn-link">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="blog-card border-box">
+                            <div class="inner-box">
+                                <div class="card">
+                                    <div class="blog-image">
+                                        <a href="#">
+                                            <img src="assets/images/work-card-img.png" alt="thumb" width="442"
+                                                height="240">
+                                        </a>
+                                    </div>
+                                    <div class="blog-content">
+                                        <div class="date">March 23, 2023</div>
+                                        <h5 class="h5 blog-title">
+                                            <a href="#">
+                                                Customized NetSuite Comparative Reports for Improved
+                                                Business Analysis
+                                            </a>
+                                        </h5>
+                                        <div class="btn-wrap">
+                                            <a href="#" class="btn-link">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="blog-card border-box">
+                            <div class="inner-box">
+                                <div class="card">
+                                    <div class="blog-image">
+                                        <a href="#">
+                                            <img src="assets/images/work-card-img.png" alt="thumb" width="442"
+                                                height="240">
+                                        </a>
+                                    </div>
+                                    <div class="blog-content">
+                                        <div class="date">March 23, 2023</div>
+                                        <h5 class="h5 blog-title">
+                                            <a href="#">
+                                                Customized NetSuite Comparative Reports for Improved
+                                                Business Analysis
+                                            </a>
+                                        </h5>
+                                        <div class="btn-wrap">
+                                            <a href="#" class="btn-link">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="blog-card border-box">
+                            <div class="inner-box">
+                                <div class="card">
+                                    <div class="blog-image">
+                                        <a href="#">
+                                            <img src="assets/images/work-card-img.png" alt="thumb" width="442"
+                                                height="240">
+                                        </a>
+                                    </div>
+                                    <div class="blog-content">
+                                        <div class="date">March 23, 2023</div>
+                                        <h5 class="h5 blog-title">
+                                            <a href="#">
+                                                Customized NetSuite Comparative Reports for Improved
+                                                Business Analysis
+                                            </a>
+                                        </h5>
+                                        <div class="btn-wrap">
+                                            <a href="#" class="btn-link">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="pagination-wrap">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="currentColor" class="svg replaced-svg">
+                                <path d="M19 12L5 12" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                                <path d="M11 6L5 12" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                                <path d="M11 18L5 12" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <span class="current">1</span>
+                    </li>
+
+                    <li>
+                        <a href="#">2</a>
+                    </li>
+                    <li>
+                        <a href="#">3</a>
+                    </li>
+                    <li>
+                        <a href="#">4</a>
+                    </li>
+                    <li>
+                        <a href="#">5</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="currentColor" class="svg replaced-svg">
+                                <path d="M5 12H19" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                                <path d="M13 18L19 12" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                                <path d="M13 6L19 12" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- inner-banner-starts -->
+    <div class="inner-banner-wrap last-sec" id="first-sec">
+        <div class="container">
+            <div class="content">
+                <div class="tag">
+                    Blog
+                </div>
+                <h1 class="h1 title">
+                    Attract <span class="gradient-text">
+                        NewLeads
+                    </span> like never before
+                </h1>
+                <p class="desc">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, esse!
+                </p>
+                <div class="btn-wrap">
+                    <a href="#second-sec" class="btn btn-primary btn-icon offset-top">
+                        Transform
+                        <svg class="icon">
+                            <use xlink:href="./assets/images/icons/svgsprite.svg#peak-icon"></use>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- inner-banner-ends -->
+
+
+
+    <?php include("include/header.php"); ?>
+</body>
+
+</html>
